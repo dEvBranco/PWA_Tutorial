e15b194512a38500f433fadc1d7fbc006dc10f7a
Eduardo Branco <emfbranco@gmail.com>
2020-12-15 17:37:04 +0000

    add 'Event'

--- sw.js
+++ sw.js
@@ -2,3 +2,14 @@
 self.addEventListener('install', evt => {
     console.log('service worker installed');
 });
+
+// Activate service worker
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+});
+
