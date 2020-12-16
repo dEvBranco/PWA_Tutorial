2c8fd663c511a30317be3f9e60c8778bdae000dc
Eduardo Branco <emfbranco@gmail.com>
2020-12-16 12:51:46 +0000

    Add Caching

--- sw.js
+++ sw.js
@@ -1,15 +1,56 @@
+const staticCacheName = 'site-static-v2'; // every time we change site add a new version
+const dynamicCache = 'site-dynamic-v1';
+
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker installed');
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
-// Activate service worker
+// Activate service worker & delete cache
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+    //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheResponse => {
+            return cacheResponse || fetch(evt.request).then(fetchResponse => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
 });
 
