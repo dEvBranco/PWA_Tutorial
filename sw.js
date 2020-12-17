1608301c855d8c5cab8ae420ea6d1a7f7641577f
Eduardo Branco <emfbranco@gmail.com>
2020-12-17 16:47:21 +0000

    Cache Fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const staticCacheName = 'site-static-v2'; // every time we change site add a new version
-const dynamicCache = 'site-dynamic-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 
 const assets = [
     '/',
@@ -11,8 +11,20 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
 ];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
 
 // install service worker
 self.addEventListener('install', evt => {
@@ -32,7 +44,7 @@ self.addEventListener('activate', evt => {
         caches.keys().then(keys => {
             //console.log(keys);
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
             );
         })
@@ -45,11 +57,16 @@ self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request).then(cacheResponse => {
             return cacheResponse || fetch(evt.request).then(fetchResponse => {
-                return caches.open(dynamicCache).then(cache => {
+                return caches.open(dynamicCacheName).then(cache => {
                     cache.put(evt.request.url, fetchResponse.clone());
+                    limitCacheSize(dynamicCacheName, 15); // never more than 15
                     return fetchResponse;
                 });
             });
+        }).catch(() => {
+            if(evt.request.url.indexOf('.html') > -1){
+            return caches.match('/pages/fallback.html');
+            }
         })
     );
 });
