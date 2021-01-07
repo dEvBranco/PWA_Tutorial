a27b70c71a2ab6ca6c200dec5ae49819f1370303
Eduardo Branco <emfbranco@gmail.com>
2021-01-07 12:16:32 +0000

    update_1.1

---
+++ public/sw.js
@@ -0,0 +1,74 @@
+const staticCacheName = 'site-static-v2'; // every time we change site add a new version
+const dynamicCacheName = 'site-dynamic-v3';
+
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
+
+// install service worker
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// Activate service worker & delete cache
+self.addEventListener('activate', evt => {
+    //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+        caches.match(evt.request).then(cacheResponse => {
+            return cacheResponse || fetch(evt.request).then(fetchResponse => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchResponse.clone());
+                    limitCacheSize(dynamicCacheName, 15); // never more than 15
+                    return fetchResponse;
+                });
+            });
+        }).catch(() => {
+            if(evt.request.url.indexOf('.html') > -1){
+            return caches.match('/pages/fallback.html');
+            }
+        })
+        );
+    }
+});
+
